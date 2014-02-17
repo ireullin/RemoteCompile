27484d34b7dd5f186f73ac7975934c4a5bce1f46
ireullin <chienheng.lin@uitox.com>
2014-02-17 18:51:42 +0800

    Leave office.

--- RemoteCompile.py
+++ RemoteCompile.py
@@ -1,7 +1,5 @@
 import sublime, sublime_plugin
-
+ 
 class RemoteCompile(sublime_plugin.TextCommand):
-	def run(self, edit):
-		#self.view.insert(edit, 0, "Hello, World!")
-		print "1111"
-
+    def run(self, edit):
+    	print "11111"
