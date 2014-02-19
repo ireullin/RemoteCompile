50480bf639a0656f3f6d6c722f8554ba469fbaa2
ireullin <chienheng.lin@uitox.com>
2014-02-19 18:05:50 +0800

    A version could upload and compile.

--- RemoteCompile.py
+++ RemoteCompile.py
@@ -5,16 +5,18 @@ import os
 import tempfile
 import popen2
 import threading
+import time
 
 
-class RemoteCompileCommand(sublime_plugin.TextCommand):
+class RemoteCompileCommand(sublime_plugin.WindowCommand):
 
-	def run(self, edit):
+	def run(self):
 		print "starting remote compile....."
 		_t = threading.Thread(target=self.runProc)
 		_t.start()
 		#_t.join()
 
+		
 
 	def runProc(self):
 
@@ -22,24 +24,77 @@ class RemoteCompileCommand(sublime_plugin.TextCommand):
 		self.port = "1104"
 		self.user = "root"
 		self.passwd = "Uitox!!)$"
+		self.cmd = "sh ./compile.sh"
 
 		self.rPath = "/var/test"
-		self.lPath = "C:\\Users\\tech0039\\AppData\\Roaming\\Sublime Text 2\\Packages\\RemoteCompile"
+		self.lPath = "C:\\MyDocument\\git\\MyLib"
 
+		self.arrSTDIN = []
+		self.arrSTDER = []
 		self.arrFiles = []
+
+		print "preparing files...."
 		self.recurrenceDir(self.lPath, self.rPath)
+		
+		print "uploading...."
 		self.generateBatch()
 		self.execPsftp()
+		
+		print "compiling...."
+		self.sshCommand( self.cmd )
+
+		print "finished"
+		sublime.set_timeout(self.callbackResult, 0)
+		
     	
+
+	def callbackResult(self):
+		t = time.time()
+		_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(t))
+		
+		_view = self.window.new_file()
+		_view.set_name("compile report " + _time )
+
+		_edit = _view.begin_edit()
+		
+		_buff = "".join(self.arrSTDIN)
+		_view.insert(_edit, 0, _buff)
+		_view.insert(_edit, 0, "====== STANDARD OUTPUT ======\n")
+		_view.insert(_edit, 0, "\n\n")
+
+		_buff = "".join(self.arrSTDER)
+		_view.insert(_edit, 0, _buff)
+		_view.insert(_edit, 0, "====== STANDARD ERROR ======\n")
+
+
+
+		_view.end_edit(_edit)
+		sublime.status_message("finished")
+
+	def sshCommand(self, cmd):
+		_cmd = "plink -ssh " + self.host + " -P " + self.port + " -l " + self.user + " -pw " + self.passwd + " cd " + self.rPath + "; " + cmd
+		print _cmd
+		r, w, e = popen2.popen3(_cmd)
+		for l in r.readlines():
+			self.arrSTDIN.append(l)
+
+		for l in e.readlines():
+			self.arrSTDER.append(l)
+
 
 	def execPsftp(self):
 
 		_cmd = "psftp -P " + self.port + " " + self.user + "@" + self.host + " -pw " + self.passwd + " -C -b \"" + self.tmpfile.name + "\" -be"
 		r, w, e = popen2.popen3(_cmd)
 		for l in r.readlines():
-			print l
+			self.arrSTDIN.append(l)
+
+		for l in e.readlines():
+			self.arrSTDER.append(l)
 
 		os.unlink(self.tmpfile.name)
+
+
 
 	def generateBatch(self):
 	
@@ -63,9 +118,7 @@ class RemoteCompileCommand(sublime_plugin.TextCommand):
 		self.arrFiles.append( "mkdir \"" + rpath + "\"" )
 		self.arrFiles.append( "cd \"" + rpath + "\"" )
 
-
 		_dirTmp = []
-		#print lpath
 		_files = dircache.listdir(lpath)
 		for f in _files:
 			if f=="." or f=="..":
